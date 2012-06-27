76a4cd4f527d3bbeeedcf8ff1303f6ae59c09e36
Lane Harrison <ltharri1@uncc.edu>
2012-06-27 00:04:24 -0400

    added naive data generator, produces valid json

---
+++ data/testdata/generator.py
@@ -0,0 +1,26 @@
+# ip: data center: 192.168.1.; office: 192.168.2.
+# port: 1-1024
+# vulnid: 0-100
+# vulntype: 'hole', 'note'
+# cvss: 0.0-10.0
+
+from random import randrange, uniform
+import json
+
+class Entry:
+  def __init__(self):
+    self.ip = "192.168."+str(randrange(1, 3))+"."+str(randrange(1, 256))
+    self.port = randrange(1, 1025)
+    self.vulnid = randrange(1, 100)
+    self.vulntype = 'hole' if randrange(1, 11) > 9 else 'note'
+    self.cvss = uniform(1, 11)
+
+lines = 600;
+
+print "["
+for i in range(lines):
+  if( not i == lines-1 ):
+    print json.dumps(vars(Entry()))+","
+  else: 
+    print json.dumps(vars(Entry()))
+print "]"
