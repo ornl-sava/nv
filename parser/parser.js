6fcf700e61e816a9b272760d78a7efe9f03dfe76
Riley Spahn <rileyspahn@gmail.com>
2012-06-27 22:35:21 -0400

    Now makes ports and scores floats

--- parser/parser.js
+++ parser/parser.js
@@ -1,3 +1,4 @@
+
 var parseNessusResult = function(nessStr){
     var scoreReg = /CVSS Base Score : (\d\.\d)/
 
@@ -7,16 +8,16 @@ var parseNessusResult = function(nessStr){
     var code = splitNess[4]
     var holeNote = splitNess[5]
     if(scoreReg.test(nessStr)){
-        var score = scoreReg.exec(nessStr)[1]
+        var score = parseFloat(scoreReg.exec(nessStr)[1])
     }
     else{
-        var score = "0.0"
+        var score = 0.0
     }
     if(portReg.test(nessStr)){
-        var port = portReg.exec(nessStr)[1]
+        var port = parseFloat(portReg.exec(nessStr)[1])
     }
     else{
-        var port = "-1"
+        var port = -1
     }
     
     return {"ip":ip,
@@ -25,3 +26,4 @@ var parseNessusResult = function(nessStr){
         "cvssScore":score,
         "port":port}
 }
+
