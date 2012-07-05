181a695f958b10a4dfc4b3f56101ce6446466aed
l9h <l9h@ornl.gov>
2012-07-05 13:18:40 -0400

    adding data and testdata.json files

--- data/testdata/generator.py
+++ data/testdata/generator.py
@@ -9,13 +9,14 @@ import json
 
 class Entry:
   def __init__(self):
-    self.ip = "192.168."+str(randrange(1, 3))+"."+str(randrange(1, 256))
-    self.port = randrange(1, 1025)
+    self.ip = "192.168."+str(randrange(1, 5))+"."+str(randrange(1, 30))
+    self.port = randrange(1, 100)
     self.vulnid = randrange(1, 100)
     self.vulntype = 'hole' if randrange(1, 11) > 9 else 'note'
     self.cvss = uniform(1, 11)
+    self.value = 1
 
-lines = 600;
+lines = 25000;
 
 print "["
 for i in range(lines):
