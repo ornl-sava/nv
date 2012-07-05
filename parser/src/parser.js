6d3c9fce3bea638313946ef06adce40c64a9f70f
Riley Spahn <spahnrb1@ornl.gov>
2012-07-05 14:49:49 -0400

    Added better commenting to the parser.

--- parser/src/parser.js
+++ parser/src/parser.js
@@ -1,5 +1,10 @@
 var sys = require("util")
 
+/**
+ * Parses a nessus result line and handles missing fields.
+ * @param nessStr - nbe result string line
+ * @return - structure containing th eip, vulnid, vulntype, cvss and port
+ */
 var parseNessusResult = function(nessStr){
     var scoreReg = /CVSS Base Score : (\d\.\d)/;
 
@@ -28,6 +33,10 @@ var parseNessusResult = function(nessStr){
         "port":port};
 }
 
+/**
+ * @param stampString - timestamp line from an NBE file.
+ * @return - milliseconds between epoch and the time in the stamp.
+ */
 var parseNessusTimeStamp = function(stampString){
     var moment = require("moment")
     var timeFormat = "ddd MMM DD HH:mm:ss YYYY"
@@ -38,15 +47,27 @@ var parseNessusTimeStamp = function(stampString){
     return time.valueOf()
 }
 
+/**
+ * @param line - line to be tested.
+ * @return - returns true if the line is a time line containing a timestamp.
+ */
 var hasTime = function(line){
     var splits = line.split("|")
     return (splits[splits.length - 2].length > 0 && splits[0] == "timestamps")
 }
 
+/**
+ * @param line - line to be tested.
+ * @return - returns true if the line is a result line and false otherwise.
+ */
 var isResult = function(line){
     return(line.split("|")[0] === "results")
 }
 
+/**
+ * @param nbe - a string representing the contents of a NBE file.
+ * @return - array where each entry is a result from the NBE file.
+ */
 var parseNBEFile = function(nbe){
     var lines = nbe.split("\n")
     var currentTime = 0
