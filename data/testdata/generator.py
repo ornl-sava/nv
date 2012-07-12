3bed5f170ba7d2b8eb10a1b5ecbf8e7b2838d8a9
l9h <l9h@ornl.gov>
2012-07-12 16:05:51 -0400

    - histogram fix for input (not test) data

--- data/testdata/generator.py
+++ data/testdata/generator.py
@@ -12,13 +12,13 @@ class Entry:
     self.ip = "192.168."+str(randrange(1, 5))+"."+str(randrange(1, 30))
     self.port = randrange(1, 100)
     self.vulnid = randrange(1, 100)
-    self.vulntype = 'hole' if randrange(1, 11) > 9 else 'note'
+    self.vulntype = 'hole' if randrange(1, 11) > 7 else 'note'
     self.cvss = uniform(1, 11)
     self.group = 'financial' if '192.168.2' in self.ip else 'dns' if '192.168.3' in self.ip else 'workstation' 
     self.criticality = randrange(3, 6) if '192.168.2' in self.ip else randrange(2, 4) if '192.168.3' in self.ip else randrange(0, 2) 
     self.value = 1
 
-lines = 100;
+lines = 1000;
 
 print "["
 for i in range(lines):
