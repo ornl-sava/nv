5024a985486697804d314e3523da8915304a9e24
l9h <l9h@ornl.gov>
2012-07-13 18:33:00 -0400

    new color scale

---
+++ paper/buildPaper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "" > fullpaper.md
+cat intro.md >> fullpaper.md
+cat RelatedWorkSectionVulnAnalysis.md >> fullpaper.md
+cat design.md >> fullpaper.md
+cat UseCase.md >> fullpaper.md
+cat conclusion.md >> fullpaper.md
