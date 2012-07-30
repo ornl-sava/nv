54fe355b82c051b056c40559a0f0cad13557e150
l9h <l9h@ornl.gov>
2012-07-30 09:04:19 -0400

    Removed paper and works-to-cite (put in situ-papers)

--- paper/buildPaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "" > fullpaper.md
-cat intro.md >> fullpaper.md
-cat RelatedWorkSectionVulnAnalysis.md >> fullpaper.md
-cat design.md >> fullpaper.md
-cat UseCase.md >> fullpaper.md
-cat conclusion.md >> fullpaper.md
