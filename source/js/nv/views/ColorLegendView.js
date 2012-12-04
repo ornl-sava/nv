75f5882a5f78c57aad03a56fd8d11d0e54db4264
Lane Harrison <ltharri1@uncc.edu>
2012-12-03 22:30:18 -0500

    color legend now displays as 10 rects

--- source/js/nv/views/ColorLegendView.js
+++ source/js/nv/views/ColorLegendView.js
@@ -8,6 +8,8 @@ var ColorLegendView = Backbone.View.extend({
   },
 
   render: function(){
+    // TODO feed color scales instead of data
+    // TODO color model should work for treemap AND here
     var data = this.model.get('data')
       , div = d3.select(this.options.target);
 
@@ -17,17 +19,21 @@ var ColorLegendView = Backbone.View.extend({
     var svgs = entries.enter().append('div')
       .classed('legendEntry', true)
       .append('svg')
-      .attr('width', 100)
-      .attr('height', 20);
+      .attr('width', 150)
+      .attr('height', 50);
 
-    svgs.append('rect')
-      .attr('width', 20)
-      .attr('height', 20)
-      .style('fill', function(d) { return d.color; });
+    svgs.selectAll('rect')
+      .data([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+      .enter().append('rect')
+      .attr('width', 15)
+      .attr('height', 15)
+      .attr('x', function(d, i) { return i*15; })
+      .style('fill', function(d) { return d3.select(this.parentNode).datum().color; })
+      .style('stroke', function(d) { return d3.select(this.parentNode).datum().color; });
 
     svgs.append('text')
-      .attr('x', 25)
-      .attr('y', 13)
+      .attr('x', 150/2)
+      .attr('y', 25)
       .text( function(d) { return d.type; } );
 
     entries.exit().remove();
