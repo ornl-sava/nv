b8364093778c3846fb6cfe0b3a2894602e7d77a3
Lane Harrison <ltharri1@uncc.edu>
2012-12-13 10:03:58 -0500

    Initial commit in pages branch

--- node_modules/launchpad/resources/phantom.js
+++
@@ -1,4 +0,0 @@
-// A PhantomJS sript that simply opens the web page passed as the command line parameter
-var page = require('webpage').create();
-page.open(phantom.args[0], function (status) {
-});
