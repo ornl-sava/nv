c7ec7538697d270d10aebb5dd48fd6ef1d6dabbb
John Goodall <jgoodall@ornl.gov>
2012-12-13 12:09:49 -0500

    working load sample data link

--- source/js/legacy.js
+++ source/js/legacy.js
@@ -393,17 +393,13 @@ var handleFileSelect = function (element) {
 };
 
 
-// the file to load is data/testNetworkOpen.nbe
-$( '#sampleDataLink' )
-.click(function() {
-
-  // TODO this is what we need to do.
-//  handleFileSelect.holder.loadFile('data/testNetworkOpen.nbe');
-
-  console.log('loaded sample data');
+// load a sample data file
+$( '#sampleDataLink' ).click(function() {
+  $.get('data/testNetworkOpen.nbe', function (data) {
+    nbeText1 = data;
+    dataLoaded('Sample data file loaded.');
+  });  
 });
-
-
 
 
 // initialization
